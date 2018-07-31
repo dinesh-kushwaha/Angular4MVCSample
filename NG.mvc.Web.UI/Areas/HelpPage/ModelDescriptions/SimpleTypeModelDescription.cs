df524c6c48c68e2887bfc8bdb1850c8b2703dcec
Dinesh Kushwaha <2kush.dinesh@gmail.com>
2018-07-31 16:59:54 +0530

    Add project files.

---
+++ NG.mvc.Web.UI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NG.mvc.Web.UI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ NG.mvc.Web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NG.mvc.Web.UI.WebApiApplication" Language="C#" %>
